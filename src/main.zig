0ca6181639ef9bb84faca2eee1a6ae9d109c403e
Justin Braben <jbraben@dmsiworks.com>
2024-02-13 11:42:14 -0700

    complete graphics context and swapchain

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,45 @@
 const std = @import("std");
 const vk = @import("vulkan");
 const glfw = @import("mach_glfw");
+const GraphicsContext = @import("graphics_context.zig").GraphicsContext;
+const Swapchain = @import("swapchain.zig").Swapchain;
+// const triangle_vert = @embedFile("../shaders/triangle_vert.spv");
+// const triangle_frag = @embedFile("../shaders/triangle_frag.spv");
 const Allocator = std.mem.Allocator;
 
 const app_name = "mach-glfw + vulkan-zig = triangle";
+
+const Vertex = struct {
+    const binding_description = vk.VertexInputBindingDescription{
+        .binding = 0,
+        .stride = @sizeOf(Vertex),
+        .input_rate = .vertex,
+    };
+
+    const attribute_description = [_]vk.VertexInputAttributeDescription{
+        .{
+            .binding = 0,
+            .location = 0,
+            .format = .r32g32_sfloat,
+            .offset = @offsetOf(Vertex, "pos"),
+        },
+        .{
+            .binding = 0,
+            .location = 1,
+            .format = .r32g32b32_sfloat,
+            .offset = @offsetOf(Vertex, "color"),
+        },
+    };
+
+    pos: [2]f32,
+    color: [3]f32,
+};
+
+const vertices = [_]Vertex{
+    .{ .pos = .{ 0, -0.5 }, .color = .{ 1, 0, 0 } },
+    .{ .pos = .{ 0.5, 0.5 }, .color = .{ 0, 1, 0 } },
+    .{ .pos = .{ -0.5, 0.5 }, .color = .{ 0, 0, 1 } },
+};
 
 /// Default GLFW error handling callback
 fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
@@ -34,5 +70,25 @@ pub fn main() !void {
     defer window.destroy();
 
     const allocator = std.heap.page_allocator;
-    _ = allocator;
+    
+    const gc = try GraphicsContext.init(allocator, app_name, window);
+    defer gc.deinit();
+
+    std.debug.print("Using device: {?s}\n", .{gc.props.device_name});
+
+    var swapchain = try Swapchain.init(&gc, allocator, extent);
+    defer swapchain.deinit();
+    
+    const pipeline_layout = try gc.vkd.createPipelineLayout(gc.dev, &.{
+        .flags = .{},
+        .set_layout_count = 0,
+        .p_set_layouts = undefined,
+        .push_constant_range_count = 0,
+        .p_push_constant_ranges = undefined,
+    }, null);
+    defer gc.vkd.destroyPipelineLayout(gc.dev, pipeline_layout, null);
+
+    while (!window.shouldClose()) {
+        glfw.pollEvents();
+    }
 }
