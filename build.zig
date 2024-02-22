8c28aac269c10660b28342ceacf54554809c4725
Justin Braben <jbraben@dmsiworks.com>
2024-02-22 11:37:00 -0700

    zigimg now linking properly

--- build.zig
+++ build.zig
@@ -16,17 +16,29 @@ pub fn build(b: *std.Build) !void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    try ensureDependencies(b.allocator);
+
     const mach_core_dep = b.dependency("mach_core", .{
         .target = target,
         .optimize = optimize,
     });
+
+    var deps = std.ArrayList(std.Build.Module.Import).init(b.allocator);
+    try deps.append(std.Build.Module.Import{
+            .name = "zigimg",
+            .module = b.createModule(.{
+                .root_source_file = .{ .path = "libs/zigimg/zigimg.zig" },
+            }),
+        });
+
     const app = try mach_core.App.init(b, mach_core_dep.builder, .{
         .name = "myapp",
         .src = "src/main.zig",
         .target = target,
         .optimize = optimize,
-        .deps = &[_]std.Build.Module.Import{},
+        .deps = deps.items,
     });
+    
     if (b.args) |args| app.run.addArgs(args);
 
     // This creates a build step. It will be visible in the `zig build --help` menu,
@@ -50,3 +62,107 @@ pub fn build(b: *std.Build) !void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 }
+
+pub fn copyFile(src_path: []const u8, dst_path: []const u8) void {
+    std.fs.cwd().makePath(std.fs.path.dirname(dst_path).?) catch unreachable;
+    std.fs.cwd().copyFile(src_path, std.fs.cwd(), dst_path, .{}) catch unreachable;
+}
+
+fn sdkPath(comptime suffix: []const u8) []const u8 {
+    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
+    return comptime blk: {
+        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
+        break :blk root_dir ++ suffix;
+    };
+}
+
+fn ensureDependencies(allocator: std.mem.Allocator) !void {
+    try optional_dependency.ensureGitRepoCloned(
+        allocator,
+        "https://github.com/slimsag/zigimg",
+        "ad6ad042662856f55a4d67499f1c4606c9951031",
+        sdkPath("/libs/zigimg"),
+    );
+}
+
+const optional_dependency = struct {
+    fn ensureGitRepoCloned(allocator: std.mem.Allocator, clone_url: []const u8, revision: []const u8, dir: []const u8) !void {
+        if (xIsEnvVarTruthy(allocator, "NO_ENSURE_SUBMODULES") or xIsEnvVarTruthy(allocator, "NO_ENSURE_GIT")) {
+            return;
+        }
+
+        xEnsureGit(allocator);
+
+        if (std.fs.openDirAbsolute(dir, .{})) |_| {
+            const current_revision = try xGetCurrentGitRevision(allocator, dir);
+            if (!std.mem.eql(u8, current_revision, revision)) {
+                // Reset to the desired revision
+                xExec(allocator, &[_][]const u8{ "git", "fetch" }, dir) catch |err| std.debug.print("warning: failed to 'git fetch' in {s}: {s}\n", .{ dir, @errorName(err) });
+                try xExec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
+                try xExec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
+            }
+            return;
+        } else |err| return switch (err) {
+            error.FileNotFound => {
+                std.log.info("cloning required dependency..\ngit clone {s} {s}..\n", .{ clone_url, dir });
+
+                try xExec(allocator, &[_][]const u8{ "git", "clone", "-c", "core.longpaths=true", clone_url, dir }, ".");
+                try xExec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
+                try xExec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
+                return;
+            },
+            else => err,
+        };
+    }
+
+    fn xExec(allocator: std.mem.Allocator, argv: []const []const u8, cwd: []const u8) !void {
+        var child = std.ChildProcess.init(argv, allocator);
+        child.cwd = cwd;
+        _ = try child.spawnAndWait();
+    }
+
+    fn xGetCurrentGitRevision(allocator: std.mem.Allocator, cwd: []const u8) ![]const u8 {
+        const result = try std.ChildProcess.run(.{ .allocator = allocator, .argv = &.{ "git", "rev-parse", "HEAD" }, .cwd = cwd });
+        allocator.free(result.stderr);
+        if (result.stdout.len > 0) return result.stdout[0 .. result.stdout.len - 1]; // trim newline
+        return result.stdout;
+    }
+
+    fn xEnsureGit(allocator: std.mem.Allocator) void {
+        const argv = &[_][]const u8{ "git", "--version" };
+        const result = std.ChildProcess.run(.{
+            .allocator = allocator,
+            .argv = argv,
+            .cwd = ".",
+        }) catch { // e.g. FileNotFound
+            std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
+            std.process.exit(1);
+        };
+        defer {
+            allocator.free(result.stderr);
+            allocator.free(result.stdout);
+        }
+        if (result.term.Exited != 0) {
+            std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
+            std.process.exit(1);
+        }
+    }
+
+    fn xIsEnvVarTruthy(allocator: std.mem.Allocator, name: []const u8) bool {
+        if (std.process.getEnvVarOwned(allocator, name)) |truthy| {
+            defer allocator.free(truthy);
+            if (std.mem.eql(u8, truthy, "true")) return true;
+            return false;
+        } else |_| {
+            return false;
+        }
+    }
+
+    fn xSdkPath(comptime suffix: []const u8) []const u8 {
+        if (suffix[0] != '/') @compileError("suffix must be an absolute path");
+        return comptime blk: {
+            const root_dir = std.fs.path.dirname(@src().file) orelse ".";
+            break :blk root_dir ++ suffix;
+        };
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,20 @@
 const std = @import("std");
 const core = @import("mach-core");
 const gpu = core.gpu;
+const zigimg = @import("zigimg");
 
 pub const App = @This();
 
 title_timer: core.Timer,
 pipeline: *gpu.RenderPipeline,
+texture: *gpu.Texture,
+texture_view: *gpu.TextureView,
+
+const sample_count = 4;
 
 pub fn init(app: *App) !void {
     try core.init(.{});
+    app.title_timer = try core.Timer.start();
 
     const shader_module = core.device.createShaderModuleWGSL("shader.wgsl", @embedFile("shader.wgsl"));
     defer shader_module.release();
@@ -31,33 +37,64 @@ pub fn init(app: *App) !void {
             .module = shader_module,
             .entry_point = "vertex_main",
         },
+        .multisample = gpu.MultisampleState{
+            .count = sample_count,
+        },
     };
-    const pipeline = core.device.createRenderPipeline(&pipeline_descriptor);
 
-    app.* = .{ .title_timer = try core.Timer.start(), .pipeline = pipeline };
+    app.pipeline = core.device.createRenderPipeline(&pipeline_descriptor);
+
+    app.texture = core.device.createTexture(&gpu.Texture.Descriptor{
+        .size = gpu.Extent3D{
+            .width = core.descriptor.width,
+            .height = core.descriptor.height,
+        },
+        .sample_count = sample_count,
+        .format = core.descriptor.format,
+        .usage = .{ .render_attachment = true },
+    });
+    app.texture_view = app.texture.createView(null);
 }
 
 pub fn deinit(app: *App) void {
     defer core.deinit();
+
     app.pipeline.release();
+    app.texture.release();
+    app.texture_view.release();
 }
 
 pub fn update(app: *App) !bool {
     var iter = core.pollEvents();
     while (iter.next()) |event| {
         switch (event) {
+            .framebuffer_resize => |size| {
+                app.texture.release();
+                app.texture = core.device.createTexture(&gpu.Texture.Descriptor{
+                    .size = gpu.Extent3D{
+                        .width = size.width,
+                        .height = size.height,
+                    },
+                    .sample_count = sample_count,
+                    .format = core.descriptor.format,
+                    .usage = .{ .render_attachment = true },
+                });
+
+                app.texture_view.release();
+                app.texture_view = app.texture.createView(null);
+            },
             .close => return true,
             else => {},
         }
     }
 
-    const queue = core.queue;
     const back_buffer_view = core.swap_chain.getCurrentTextureView().?;
     const color_attachment = gpu.RenderPassColorAttachment{
-        .view = back_buffer_view,
+        .view = app.texture_view,
+        .resolve_target = back_buffer_view,
         .clear_value = std.mem.zeroes(gpu.Color),
         .load_op = .clear,
-        .store_op = .store,
+        .store_op = .discard,
     };
 
     const encoder = core.device.createCommandEncoder(null);
@@ -73,15 +110,15 @@ pub fn update(app: *App) !bool {
     var command = encoder.finish(null);
     encoder.release();
 
+    const queue = core.queue;
     queue.submit(&[_]*gpu.CommandBuffer{command});
     command.release();
     core.swap_chain.present();
     back_buffer_view.release();
 
-    // update the window title every second
     if (app.title_timer.read() >= 1.0) {
         app.title_timer.reset();
-        try core.printTitle("Triangle [ {d}fps ] [ Input {d}hz ]", .{
+        try core.printTitle("Triangle MSAA [ {d}fps ] [ Input {d}hz ]", .{
             core.frameRate(),
             core.inputRate(),
         });
