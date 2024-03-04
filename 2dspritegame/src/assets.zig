80263ddf6ee57bcbe94281cff37f4c1e2798f1f5
Justin Braben <jbraben@dmsiworks.com>
2024-03-04 11:06:31 -0700

    Moving files around

--- 2dspritegame/src/assets.zig
+++ 2dspritegame/src/assets.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 // Relative to root folder
 // Use with std.fs.cwd().openFile(assets.some_file_path)
 const root_path = "assets/";
-const sprites_path = root_path ++ "characters/";
+const sprites_path = root_path ++ "images/";
 
-pub const sprites_sheet_png = @embedFile("assets/characters/sheet.png");
-pub const animation_sheet_png = @embedFile("assets/characters/AnimationSheet_Character.png");
+pub const sprites_sheet_png = @embedFile("assets/images/sheet.png");
+pub const animation_sheet_png = @embedFile("assets/images/AnimationSheet_Character.png");
