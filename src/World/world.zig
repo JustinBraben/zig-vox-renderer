96c1dd14106730c140e84ff3fd6bf43eea9c0f2c
JustinBraben <brabenj1996@gmail.com>
2025-01-02 10:16:00 -0700

    trying out 3d noise

--- src/World/world.zig
+++ src/World/world.zig
@@ -32,17 +32,33 @@ pub fn init(gpa: Allocator, height_range: f32) !World {
 }
 
 pub fn generate(self: *World) !void {
-    for (0..1000) |x_pos| {
-        for (0..1000) |z_pos| {
-            const x = @mod(@as(f32, @floatFromInt(x_pos)), 1000.0);
-            const z = @mod(@as(f32, @floatFromInt(z_pos)), 1000.0);
+    // for (0..1000) |x_pos| {
+    //     for (0..1000) |z_pos| {
+    //         const x = @mod(@as(f32, @floatFromInt(x_pos)), 1000.0);
+    //         const z = @mod(@as(f32, @floatFromInt(z_pos)), 1000.0);
 
-            const y = @floor(self.gen.noise2(x, z) * self.height_range);
+    //         const y = @floor(self.gen.noise2(x, z) * self.height_range);
 
-            // const position = zm.translation(x, y, z);
-            try self.model_matrices.append(zm.matToArr(zm.translation(x, y, z)));
+    //         // const position = zm.translation(x, y, z);
+    //         try self.model_matrices.append(zm.matToArr(zm.translation(x, y, z)));
+    //     }
+    // }
+
+    for (0..200) |x_pos| {
+        for (0..100) |y_pos| {
+            for (0..10) |z_pos| {
+                const x = @mod(@as(f32, @floatFromInt(x_pos)), 1000.0);
+                const y = @mod(@as(f32, @floatFromInt(y_pos)), 1000.0);
+                const z = @mod(@as(f32, @floatFromInt(z_pos)), 1000.0);
+                
+                const noise = self.gen.noise3(x, y, z);
+                if (noise > 0) {
+                    try self.model_matrices.append(zm.matToArr(zm.translation(x, y, z)));
+                }
+            }
         }
     }
+
     self.flattened_matrices = try Utils.flattenMatrices(self.model_matrices.items, self.allocator);
 }
 
